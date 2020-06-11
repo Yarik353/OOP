1aac93cbf837b23561112a02931ae8670f3334c4
Yarik353 <65865743+Yarik353@users.noreply.github.com>
2020-06-11 11:44:01 +0300

    Changed Naming

--- lab7/PlaneSet.java
+++ lab7/PlaneSet.java
@@ -7,36 +7,28 @@ import java.util.*;
 
 public class PlaneSet implements Set<Plane> {
 
-    private List s = new List();
+    private List list = new List();
     private int size = 0;
 
     public PlaneSet() {
     }
 
-    public PlaneSet(Plane stn) {
-        add(stn);
+    public PlaneSet(Plane obj) {
+        add(obj);
     }
 
-    public PlaneSet(Collection<? extends Plane> stn) {
-        addAll(stn);
+    public PlaneSet(Collection<? extends Plane> c) {
+        addAll(c);
     }
 
     @Override
-    public boolean add(Plane stn) {
-        boolean flag = true;
-        Node t = s.getHead();
-        while (t != null) {
-            if (t.data == stn) {
-                flag = false;
-                break;
-            }
-            t = t.next;
-        }
-        if (flag) {
-            s.addBack(stn);
+    public boolean add(Plane obj) {
+        boolean isContains = contains((Plane)obj);
+        if (!isContains) {
+            list.addBack(obj);
             size++;
         }
-        return flag;
+        return !isContains;
     }
 
     @Override
@@ -52,25 +44,30 @@ public class PlaneSet implements Set<Plane> {
     @Override
     public void clear() {
         size = 0;
-        s = new List();
+        list = new List();
     }
 
     @Override
     public boolean contains(Object o) {
-        boolean flag = add((Plane) o);
-        if (flag) {
-            s.delEl((Plane) o);
+        boolean flag = false;
+        Node t = list.getHead();
+        while (t != null) {
+            if (t.data == o) {
+                flag = true;
+                break;
+            }
+            t = t.next;
         }
-        return !flag;
+        return flag;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        boolean flag = true;
+        boolean flag = false;
         Iterator<Plane> iterator = (Iterator<Plane>) c.iterator();
         while (iterator.hasNext()) {
             flag = contains((Plane) iterator.next());
-            if (flag == false) {
+            if (!flag) {
                 break;
             }
         }
@@ -79,7 +76,7 @@ public class PlaneSet implements Set<Plane> {
 
     @Override
     public boolean isEmpty() {
-        return s.getHead() == null;
+        return list.getHead() == null;
     }
 
     @Override
@@ -90,7 +87,7 @@ public class PlaneSet implements Set<Plane> {
     @Override
     public boolean remove(Object o) {
         if (contains(o)) {
-            s.delEl((Plane) o);
+            list.delEl((Plane) o);
             size--;
             return true;
 
@@ -101,12 +98,12 @@ public class PlaneSet implements Set<Plane> {
 
     @Override
     public boolean removeAll(Collection<?> c) {
-
-        boolean flag = false;
+        boolean changed = false;
         for (Object object : c) {
-            flag = remove(object);
+            remove(object);
+            changed = true;
         }
-        return flag;
+        return changed;
     }
 
     @Override
@@ -116,10 +113,10 @@ public class PlaneSet implements Set<Plane> {
         while (iterator.hasNext()) {
             Plane Plane = (Plane) iterator.next();
             flag = c.contains(Plane);
-            if (flag == false)
+            if (!flag)
                 remove(Plane);
         }
-        return flag == false;
+        return !flag;
     }
 
     @Override
@@ -130,14 +127,12 @@ public class PlaneSet implements Set<Plane> {
     @Override
     public Object[] toArray() {
         int i = 0;
-        Plane[] mas = new Plane[size];
+        Plane[] array = new Plane[size];
         Iterator<Plane> iterator = iterator();
         while (iterator.hasNext()) {
-            mas[i] = iterator.next();
-            i++;
-
+            array[i++] = iterator.next();
         }
-        return mas;
+        return array;
     }
 
     @Override
@@ -146,7 +141,7 @@ public class PlaneSet implements Set<Plane> {
     }
 
     class PlaneIterator implements Iterator {
-        private Node thisElement = s.getHead();
+        private Node thisElement = list.getHead();
 
         @Override
         public boolean hasNext() {
@@ -155,14 +150,14 @@ public class PlaneSet implements Set<Plane> {
 
         @Override
         public Plane next() {
-            Plane s = thisElement.data;
+            Plane list = thisElement.data;
             thisElement = thisElement.next;
-            return s;
+            return list;
         }
 
         @Override
         public void remove() {
-            s.delEl(thisElement.data);
+            list.delEl(thisElement.data);
         }
     }
 
